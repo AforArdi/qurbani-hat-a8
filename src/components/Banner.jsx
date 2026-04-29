022fcd6e4a2a1bdf57ed2aefbf63174ca9a7ff99
AforArdi <binsbro36@gmail.com>
2026-04-30 00:11:50 +0600

    banner section browse buton added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import banner from '@/assets/banner.png'
+import Link from "next/link";
+import { GiCow } from "react-icons/gi";
 
 const Banner = () => {
     return ( 
@@ -7,6 +9,9 @@ const Banner = () => {
             <div className="flex flex-col justify-center gap-4">
                 <h2 className="text-[#154734] text-5xl font-bold leading-snug">Book Your Qurbani, <br /> Fulfill Your Faith</h2>
                 <p>Choose from healthy livestock: Cows, Bulls, Goats & Sheep <br /> and book online with ease, trust & transparency.</p>
+                <Link href={'/animals'}>
+                    <button className="btn bg-[#154734] text-white rounded-3xl"><GiCow size={30}></GiCow> Browse</button>
+                </Link>
             </div>
             <Image src={banner} alt="Banner Image" width={420} height={300}
             className="rounded-3xl"
