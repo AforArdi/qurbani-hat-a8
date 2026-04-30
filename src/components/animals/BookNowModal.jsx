e41d0e617075f688b792b46262cec1ae51bf9be0
AforArdi <binsbro36@gmail.com>
2026-04-30 16:50:11 +0600

    modal form btn error

--- src/components/animals/BookNowModal.jsx
+++ src/components/animals/BookNowModal.jsx
@@ -2,12 +2,22 @@
 
 import { Button, Input, Label, Modal, Surface, TextField, toast } from "@heroui/react";
 import { MdOutlineBorderColor } from "react-icons/md";
+import { useForm } from "react-hook-form"
 
 const BookNowModal = () => {
-    const handleBookNow = (e) => {
-        e.preventDefault(); // stop page reload
-        alert('Your Booking is confirmed!');
-    };
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+    
+    const onSubmit = (data) => {
+        if(!error){
+            alert('Booking Done')
+        }
+    }
+
     return (
         <Modal>
             <Button variant="primary">Book Now</Button>
@@ -26,29 +36,33 @@ const BookNowModal = () => {
                         </Modal.Header>
                         <Modal.Body className="p-6">
                             <Surface variant="default">
-                                <form onSubmit={handleBookNow} className="flex flex-col gap-4">
-                                    <TextField className="w-full" name="name" type="text">
+                                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                                    <TextField className="w-full" type="text">
                                         <Label>Name</Label>
-                                        <Input placeholder="Enter your name" />
+                                        <Input placeholder="Enter your name"
+                                        {...register("name", { required: 'name is required' })} />
                                     </TextField>
-                                    <TextField className="w-full" name="email" type="email">
+                                    <TextField className="w-full" type="email">
                                         <Label>Email</Label>
-                                        <Input placeholder="Enter your email" />
+                                        <Input placeholder="Enter your email"
+                                        {...register("email", { required: 'email is required' })} />
                                     </TextField>
-                                    <TextField className="w-full" name="phone" type="tel">
+                                    <TextField className="w-full" type="tel">
                                         <Label>Phone</Label>
-                                        <Input placeholder="Enter your phone number" />
+                                        <Input placeholder="Enter your phone number"
+                                        {...register("phone", { required: 'phone is required' })} />
                                     </TextField>
                                     <TextField className="w-full" name="address">
                                         <Label>Address</Label>
-                                        <Input placeholder="Enter your address" />
+                                        <Input placeholder="Enter your address"
+                                        {...register("address", { required: 'address is required' })} />
                                     </TextField>
                                 </form>
                                 <Modal.Footer className="mt-4">
                                     <Button slot="close" variant="secondary">
                                         Cancel
                                     </Button>
-                                    <Button slot="close">Confirm Booking</Button>
+                                    <Button type="submit">Confirm Booking</Button>
                                 </Modal.Footer>
                             </Surface>
                         </Modal.Body>
