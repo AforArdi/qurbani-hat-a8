aeb2818ed022397fc34465f874372e4342a1ebe3
AforArdi <binsbro36@gmail.com>
2026-04-30 11:01:46 +0600

    working on card

--- src/components/animals/AnimalCard.jsx
+++ src/components/animals/AnimalCard.jsx
@@ -1,9 +1,10 @@
-import { Card } from "@heroui/react";
+import { Button, Card } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 import defaultPic from '@/assets/qurbaniAnimalsDefaultPic.png'
 import { FaLocationDot } from "react-icons/fa6";
 import { TbCoinTaka } from "react-icons/tb";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const AnimalCard = ({ animal }) => {
     return (
@@ -17,12 +18,14 @@ const AnimalCard = ({ animal }) => {
                         {animal.name}
                     </Card.Title>
                     <Card.Description className="flex items-center justify-between">
-                        <p className="flex items-center text-xl">Price: <TbCoinTaka></TbCoinTaka>{animal.price}</p>
-                        <p className="flex items-center text-xl"><FaLocationDot size={15}></FaLocationDot> {animal.location}</p>
+                        <span className="flex items-center text-xl">Price: <TbCoinTaka></TbCoinTaka>{animal.price}</span>
+                        <span className="flex items-center text-xl"><FaLocationDot size={15}></FaLocationDot> {animal.location}</span>
                     </Card.Description>
                 </Card.Header>
-                <Card.Footer>
-                    <h2>hi</h2>
+                <Card.Footer className="w-full">
+                    <Link href={'/animals'}>
+                        <button className="btn rounded-3xl bg-[#154734] text-white w-full">View Details <FaLongArrowAltRight></FaLongArrowAltRight></button>
+                    </Link>
                 </Card.Footer>
             </Card>
         </div>
