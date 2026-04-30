5508f659c66cd5bfeb7591e2d526c8642e9b42b0
AforArdi <binsbro36@gmail.com>
2026-05-01 00:01:31 +0600

    working on profile card on profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,32 @@
+'use client'
+import { Card, Separator } from "@heroui/react";
+import Image from "next/image";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoIosPricetag } from "react-icons/io";
+import { authClient } from "@/lib/auth-client";
+
 const ProfilePage = () => {
-    return ( 
-        <div className="">
-            <h2 className="text-3xl font-bold text-[#154734]">Login Your Account</h2>
+    const { data: session } = authClient.useSession();
+    const user = session?.user;
+    console.log(user);
+
+    return (
+        <div className="max-w-6xl mx-auto my-10">
+            <Card className="w-[80%] mx-auto items-stretch md:flex-row">
+                <h2 className="text-3xl font-bold text-[#154734]">Login Your Account</h2>
+                <Image src={user.image} alt={user.name} width={200} height={200}
+                    className="rounded-full"
+                ></Image>
+                <div className="flex flex-1 flex-col gap-3 space-y-3">
+                    <Card.Header className="gap-1 space-y-3 mb-auto">
+                        <Card.Title className="pr-8 font-bold text-3xl text-[#154734]">{user.name}</Card.Title>
+                    </Card.Header>
+                    <Card.Footer className="flex w-full flex-col items-start gap-3 sm:flex-row sm:items-center sm:justify-between">
+                    </Card.Footer>
+                </div>
+            </Card>
         </div>
-     );
+    );
 }
- 
+
 export default ProfilePage;
