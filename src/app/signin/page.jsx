f2b88a24ba770aa53c2c8a3302216a41e3f1cfe0
AforArdi <binsbro36@gmail.com>
2026-05-01 01:28:41 +0600

    google login implemented, but need to work on the design, working on profile page errors

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -3,6 +3,7 @@
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 import { useForm } from "react-hook-form"
+import { FaGoogle } from "react-icons/fa6";
 import { toast } from "react-toastify";
 
 const SignInPage = () => {
@@ -30,28 +31,37 @@ const SignInPage = () => {
             toast.error(`${error.message}`);
         }
     }
+    const handleGoogleSignInBtn = async () => {
+        await authClient.signIn.social({
+            provider: "google",
+        });
+    }
     return (
         <div className="flex flex-col justify-center items-center gap-5 my-10">
             <h2 className="text-3xl font-bold text-[#154734]">Login Your Account</h2>
             <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                onSubmit={handleSubmit(onSubmit)}
+                className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 
                 <label className="label">Email</label>
                 <input type="email"
-                {...register("email", { required: 'Email is required' })}
-                className="input" placeholder="Email" />
+                    {...register("email", { required: 'Email is required' })}
+                    className="input" placeholder="Email" />
                 {errors.email && <span>{errors.email.message}</span>}
 
                 <label className="label">Password</label>
                 <input type="password"
-                {...register("password", { required: 'Password is required', minLength: {value: 8, message: 'Minimum Length is 8'} })}
-                className="input" placeholder="Password" />
+                    {...register("password", { required: 'Password is required', minLength: { value: 8, message: 'Minimum Length is 8' } })}
+                    className="input" placeholder="Password" />
                 {errors.password && <span>{errors.password.message}</span>}
 
                 <button className="btn bg-[#154734] text-white rounded-3xl mt-4">Sign in</button>
                 <p className="text-[14px]">Don't have any account? <Link href={'/signup'} className="text-[#154734]">Sign up</Link></p>
             </form>
+            <div className="flex flex-col items-center">
+                <p>or</p>
+                <button onClick={handleGoogleSignInBtn} className="btn border border-blue-500 text-blue-500 rounded-3xl mt-4"><FaGoogle></FaGoogle> Continue with Google</button>
+            </div>
         </div>
     );
 }
