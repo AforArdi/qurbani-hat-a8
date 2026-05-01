333d3a7a298b808e37717e66e09222717b22fd86
AforArdi <binsbro36@gmail.com>
2026-05-02 01:26:24 +0600

    lotti added to banner

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -2,11 +2,22 @@ import Image from "next/image";
 import banner from '@/assets/banner.png'
 import Link from "next/link";
 import { GiCow } from "react-icons/gi";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import animationData from '@/assets/eid-animation.json';
 
 const Banner = () => {
     return ( 
         <div className="max-w-6xl mx-auto mt-5 flex items-center justify-between">
             <div className="flex flex-col justify-center gap-4">
+                {/* lotti welcome animation */}
+                <div className="w-50 h-50 -mb-6"> 
+                    <DotLottieReact 
+                        data={animationData} 
+                        loop 
+                        autoplay 
+                    />
+                </div>
+
                 <h2 className="text-[#154734] text-5xl font-bold leading-snug">Book Your Qurbani, <br /> Fulfill Your Faith</h2>
                 <p>Choose from healthy livestock: Cows, Bulls, Goats & Sheep <br /> and book online with ease, trust & transparency.</p>
                 <Link href={'/animals'}>
