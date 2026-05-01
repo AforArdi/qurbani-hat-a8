5d05059a0f81e153f6bfbd99f7ed15e2667f4b2c
AforArdi <binsbro36@gmail.com>
2026-05-01 17:46:02 +0600

    sorting problem solved, now trying to add the modal

--- src/components/animals/SortingByModal.jsx
+++ src/components/animals/SortingByModal.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Dropdown, Label } from "@heroui/react";
+
 import { useEffect, useState } from "react";
 import AnimalCard from "./AnimalCard";
 
@@ -8,35 +8,32 @@ const SortingByModal = ({ animals }) => {
     const [sortedData, setSortedData] = useState(animals);
 
     useEffect(() => {
-        if (sortingType === 'low') {
-            const sortedAnimals = [...sortedData].sort((a, b) => a.price - b.price)
-            setSortedData(sortedAnimals)
-        } else if (sortingType === 'high') {
-            const sortedAnimals = [...sortedData].sort((a, b) => b.price - a.price)
-            setSortedData(sortedAnimals)
-        } else if (sortingType === 'all') {
-            setSortedData(sortedData)
+        if (sortingType) {
+            if (sortingType === 'low') {
+                const sortedData = [...animals].sort((a, b) => a.price - b.price);
+                setSortedData(sortedData);
+            } else if (sortingType === 'high') {
+                const sortedData = [...animals].sort((a, b) => b.price - a.price);
+                setSortedData(sortedData);
+            } else if (sortingType === 'all') {
+                setSortedData(animals);
+            }
         }
-    }, [sortedData])
+    }, [sortingType, animals])
 
     return (
         <div className="max-w-6xl mx-auto my-10">
             <h2 className="text-4xl font-bold text-[#154734] text-center my-5">Qurbani Livestock Collection</h2>
             {/* sorting modal */}
             <div className="ml-10 mb-3">
-                <Dropdown>
-                    <Button aria-label="Menu" variant="secondary">
-                        Sort By
-                    </Button>
-                    <Dropdown.Popover>
-                        {/* onAction={(key) => setSortingType(key)} */}
-                        <Dropdown.Menu onAction={(key) => setSortingType(key)}>
-                            <Dropdown.Item key="all">All</Dropdown.Item>
-                            <Dropdown.Item key="low">Price low to high</Dropdown.Item>
-                            <Dropdown.Item key="high">Price high to low</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown.Popover>
-                </Dropdown>
+                <details className="dropdown">
+                    <summary className="btn m-1">Sort By</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li onClick={() => setSortingType('all')}><a>All</a></li>
+                        <li onClick={() => setSortingType('low')}><a>Price Low to High</a></li>
+                        <li onClick={() => setSortingType('high')}><a>Price High to Low</a></li>
+                    </ul>
+                </details>
             </div>
             {/* main mapping */}
             <div className="grid grid-cols-3 gap-6">
