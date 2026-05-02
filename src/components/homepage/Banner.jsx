7f8aa6a225d5460f8b4038a104f5143149df14d9
AforArdi <binsbro36@gmail.com>
2026-05-02 16:21:38 +0600

    banner jsx updated, responsive

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -7,17 +7,16 @@ import animationData from '@/assets/eid-animation.json';
 
 const Banner = () => {
     return ( 
-        <div className="max-w-6xl mx-auto mt-5 flex items-center justify-between">
-            <div className="flex flex-col justify-center gap-4">
+        <div className="max-w-6xl mx-auto mt-5 px-4 flex flex-col lg:flex-row items-center justify-between gap-10 lg:gap-0">
+            <div className="flex flex-col justify-center items-center text-center lg:text-left lg:items-start gap-4">
                 {/* lotti welcome animation */}
-                <div className="w-50 h-50 -mb-6"> 
+                <div className="w-50 h-50 -mb-6 self-center"> 
                     <DotLottieReact 
                         data={animationData} 
                         loop 
                         autoplay 
                     />
                 </div>
-
                 <h2 className="text-[#154734] text-5xl font-bold leading-snug">Book Your Qurbani, <br /> Fulfill Your Faith</h2>
                 <p>Choose from healthy livestock: Cows, Bulls, Goats & Sheep <br /> and book online with ease, trust & transparency.</p>
                 <Link href={'/animals'}>
